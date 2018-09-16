5d88ea741c32ebf52b37fddfba32f62d1a73ff7d
Student <www.vrushu20@gmail.com>
2018-09-16 11:20:09 +0530

    added test.sh file

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "making foss folder"
+mkdir foss
+
+echo "going to foss folder"
+cd foss
+
+echo"creating test_file.txt"
+touch test test_file.txt
+
+echo""
+echo "showing output ofb ls:"
+ls
+
+echo""
+echo "updating linux redos"
